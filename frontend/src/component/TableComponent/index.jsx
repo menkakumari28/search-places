80bfe2a92424108a1df9daeaf50adf6044b4a0a5
menka.kumari <menka.kumari@paytm.com>
2024-09-30 22:06:55 +0530

    Updated css and handles minor errors

--- frontend/src/component/TableComponent/index.jsx
+++ frontend/src/component/TableComponent/index.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
 import data from './data.json';
 import { Spin } from 'antd';
-import './index.css';
+import './style.css';
 const TableComponent = () => {
     const [searchPlace, setSearchPlace] = useState();
     const inputRef = useRef(undefined);
     const [places, setPlaces] = useState(data);
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
+    const [isDisabled, setIsDisabled] = useState(false)
     const callSearchApi = (params) => {
         const filteredData = data.data.filter((data) => {
             return data.city?.toLowerCase() === params.toLowerCase();
@@ -48,6 +49,7 @@ const TableComponent = () => {
     // }, [places]);
 
     useEffect(() => {
+        // This is handled only for windows ctrl + / 
         const handleClick = (e) => {
             if (e.ctrlKey) {
                 if (e.key?.toLowerCase() === 'k') {
@@ -64,18 +66,21 @@ const TableComponent = () => {
     }, []);
 
     return (
-        <>
-            <input
-                type="text"
-                id="myInput"
-                ref={inputRef}
-                onKeyDown={handleKeyDown}
-                onChange={(e) => setSearchPlace(e.target.value)}
-                name="search"
-                value={searchPlace}
-                placeholder="Search places..."
-                autoComplete="off"
-            />
+        <div className="main">
+            <div className="form-group">
+                <input
+                    type="text"
+                    ref={inputRef}
+                    onKeyDown={handleKeyDown}
+                    disabled={isDisabled}
+                    onChange={(e) => setSearchPlace(e.target.value)}
+                    name="search"
+                    value={searchPlace}
+                    placeholder="Search places..."
+                    autoComplete="off"
+                />
+                <span>Ctrl + /</span>
+            </div>
             {error && 'Something went wrong..'}
             {!error && (
                 <table id="myTable">
@@ -107,35 +112,29 @@ const TableComponent = () => {
                             <div>No Records</div>
                         )}
                     </tbody>
-                    <tfoot>
-                        {places.data.length > 0 && (
-                            <div className="pagination" id="pagination">
-                                <a href="#" id="prev">
-                                    Previous
-                                </a>
-                                <a href="#" className="page-link" data-page="1">
-                                    1
-                                </a>
-                                <a href="#" className="page-link" data-page="2">
-                                    2
-                                </a>
-                                <a href="#" className="page-link" data-page="3">
-                                    3
-                                </a>
-                                <a href="#" id="next">
-                                    Next
-                                </a>
-                                <p id="page-numbers"> </p>
-                                <select id="pagination-option">
-                                    <option>5</option>
-                                    <option>10</option>
-                                </select>
-                            </div>
-                        )}
-                    </tfoot>
                 </table>
             )}
-        </>
+            <div>
+                {places.data.length > 0 && (
+                    <div className="pagination" id="pagination">
+                        <a href="#" id="prev">
+                            Previous
+                        </a>
+                        <a href="#" className="page-link" data-page="1">
+                            1
+                        </a>
+                        <a href="#" id="next">
+                            Next
+                        </a>
+                        <p id="page-numbers"> </p>
+                        <select id="pagination-option">
+                            <option>5</option>
+                            <option>10</option>
+                        </select>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 };
 
